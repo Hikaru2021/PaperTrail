802cc8965b0826b9bc55b096228c108c1a76ce08
Jose Mari L. Hinolan <josehinvauj@gmail.com>
2023-07-14 19:22:15 +0800

    update2

---
+++ src/components/SignUpModal.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="fixed inset-0 flex items-center justify-center z-50">
+      <div class="fixed inset-0 bg-black opacity-70"></div> <!-- Modal overlay -->
+      <div class="bg-color p-14 h-3/5 w-96 rounded-md shadow-md relative">
+        <button class="absolute top-2 right-2 p-2 bg-" @click="$emit('close-modal')">
+            <img src="/x-icon.svg" alt="SVG Image" class="h-4"/>
+        </button>
+        <h1 class="text-4xl text-white flex justify-center items-center mb-16">Sign Up!</h1>
+        <h1 class="text-sky-600">Email</h1>
+        <h1 class="text-sky-600">Username</h1>
+        <h1 class="text-sky-600">Password</h1>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+    .bg-image {
+      background-image: url('/bg-landing.jpg');
+      background-size: cover;
+    }
+
+    .bg-color{
+      background-color: #161B33;
+    }
+
+    .bg-button
+    {
+      background-color: #474973;
+    }
+  </style>
+  
--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,31 +1,56 @@
 <template>
-    <div class="h-screen bg-sky-950">
-      <div class="bg-image absolute inset-0 opacity-30"></div>
-      <div class="relative bg-sky-950 opacity-90">
-        <nav class="h-14">
-          <div class="grid grid-cols-2 h-full">
-            <div class="w-full flex items-start justify-start p-4">
-              <img src="/Logo-01.png" alt="Logo" class="w-56 h-6 mt-1" />
-            </div>
-            <div class="w-full flex items-end justify-end p-4 text-white">
-              <button class="flex items-center p-2 text-sm hover:underline-offset-1">Sign In</button>
-              <button class="bg-blue-500 flex items-center justify-center p-2 rounded-full text-sm w-36 ml-3">Get Started for Free</button>
-            </div>
+  <div class="h-screen bg-color">
+    <div class="bg-image absolute inset-0 opacity-60"></div>
+    <div class="relative bg-color opacity-90">
+      <nav class="h-14">
+        <div class="grid grid-cols-2 h-full">
+          <div class="w-full flex items-start justify-start p-4">
+            <img src="/Logo-01.png" alt="Logo" class="w-56 h-6 mt-1" />
           </div>
-        </nav>
-        <div class="h-full">
-          <div class="h-screen">
-            hello
+          <div class="w-full flex items-end justify-end p-4 text-white">
+            <button class="flex items-center p-2 text-sm hover:underline-offset-1">Sign In</button>
+            <button @click="showModal = true" class="bg-button flex items-center justify-center p-2 rounded-full text-sm w-36 ml-3">Get Started for Free</button>
           </div>
         </div>
+      </nav>
+      <div class="h-screen bg-color">
+        
       </div>
     </div>
-  </template>
-  
-  <style scoped>
+  </div>
+  <div class="h-screen">
+    <SignUpModal v-if="showModal" @close-modal="showModal = false" />
+  </div>
+</template>
+
+<script>
+import SignUpModal from '@/components/SignUpModal.vue';
+
+export default {
+  components: {
+    SignUpModal
+  },
+  data() {
+    return {
+      showModal: false
+    };
+  }
+};
+</script>
+
+<style scoped>
     .bg-image {
       background-image: url('/bg-landing.jpg');
       background-size: cover;
+    }
+
+    .bg-color{
+      background-color: #161B33;
+    }
+
+    .bg-button
+    {
+      background-color: #474973;
     }
   </style>
   
